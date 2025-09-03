4a2edc32701bf9edbef9ef15ec4b6ad038e2f13a
FL <baalismlin@gmail.com>
2025-09-03 08:11:54 +0000

    Variables and Mutability

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+  let mut x: u32 = 4;
+  println!("The value of x is: {x}");
+
+  x = 29;
+  println!("The value of x is: {x}");
+
+  const COSNT_Y: i32 = 200;
+  print!("The value of y is: {COSNT_Y}");
+
+
+}
